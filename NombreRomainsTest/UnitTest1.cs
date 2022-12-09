e5e78fa838afc96b337d0975d4b7d4af27381c5c
VentoAureo230 <anthony.meny35@gmail.com>
2022-12-09 09:58:38 +0100

    Ajout des test 1, 2 et 3 fonctionnel

--- NombreRomainsTest/UnitTest1.cs
+++ NombreRomainsTest/UnitTest1.cs
@@ -14,5 +14,27 @@ namespace NombreRomainsTest
             // ALORS on obtient I
             Assert.Equal("I", attendu);
         }
+
+        [Fact]
+        public void Convertir2enII()
+        {
+            // ETANT DONNE un nombre 2
+            const int nombre = 2;
+            // QUAND on le converti en nombre romain
+            var attendu = ConvertisseurNombresRomains.Convert(nombre);
+            // ALORS on obtient I
+            Assert.Equal("Ii", attendu);
+        }
+
+        [Fact]
+        public void Convertir3enIII()
+        {
+            // ETANT DONNE un nombre 3
+            const int nombre = 3;
+            // QUAND on le converti en nombre romain
+            var attendu = ConvertisseurNombresRomains.Convert(nombre);
+            // ALORS on obtient I
+            Assert.Equal("III", attendu);
+        }
     }
 }
